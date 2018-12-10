a88dbafce044e0f34f289734dc5122a4b4966b73
Narek <narek@omplanet.net>
2018-12-10 02:21:38 +0100

    Many changes of css styles and content.

--- www/js/geometry.js
+++ www/js/geometry.js
@@ -1,6 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
-    init();
-    animate();
+    if (window.innerWidth > 700) {
+        init();
+        animate();
+    }
+
+    setTimeout(function() {
+        document.getElementById("canvas_three").classList.add('animated')
+    }, 200);
+
 }, !1)
 
 function init() {
@@ -12,7 +19,7 @@ function init() {
         antialias: !0,
         alpha: !0
     })).setPixelRatio(window.devicePixelRatio ? window.devicePixelRatio : 1), renderer.setSize(window.innerWidth, window.innerHeight), renderer.autoClear = !1, renderer.setClearColor(0, 0), document.getElementById("canvas_three").appendChild(renderer.domElement), scene = new THREE.Scene, (camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1e3)).position.z = 400, scene.add(camera), circle = new THREE.Object3D, skelet = new THREE.Object3D, particle = new THREE.Object3D, scene.add(circle), scene.add(skelet), scene.add(particle);
-    for (var e = new THREE.TetrahedronGeometry(2, 0), n = new THREE.TetrahedronGeometry(5, 0), t = new THREE.IcosahedronGeometry(15, 0), o = new THREE.MeshPhongMaterial({
+    for (var e = new THREE.TetrahedronGeometry(2, 0), n = new THREE.TetrahedronGeometry(2.3, 3), t = new THREE.IcosahedronGeometry(15, 0), o = new THREE.MeshPhongMaterial({
             color: 16777215,
             shading: THREE.FlatShading
         }), r = 0; r < 20; r++) {
@@ -35,7 +42,7 @@ function init() {
     var d = new THREE.AmbientLight(6710886);
     scene.add(d);
     var u = [];
-    u[0] = new THREE.DirectionalLight(16777215, 1), u[0].position.set(1, 0, 0), u[1] = new THREE.DirectionalLight(15728895, 1), u[1].position.set(.75, 1, .5), u[2] = new THREE.DirectionalLight(3961855, 1), u[2].position.set(-.75, -1, .5), scene.add(u[0]), scene.add(u[1]), scene.add(u[2]), window.addEventListener("resize", onWindowResize, !1)
+    u[0] = new THREE.DirectionalLight(16777215, 1), u[0].position.set(1, 0, 0), u[1] = new THREE.DirectionalLight(16759826, 1), u[1].position.set(.75, 1, .5), u[2] = new THREE.DirectionalLight(13077504, 1), u[2].position.set(-.75, -1, .5), scene.add(u[0]), scene.add(u[1]), scene.add(u[2]), window.addEventListener("resize", onWindowResize, !1)
 }
 
 function onWindowResize() {
@@ -44,8 +51,4 @@ function onWindowResize() {
 
 function animate() {
     requestAnimationFrame(animate), particle.rotation.x += 0, particle.rotation.y -= .004, circle.rotation.x -= .002, circle.rotation.y -= .003, skelet.rotation.x -= .001, skelet.rotation.y += .002, renderer.clear(), renderer.render(scene, camera)
-
-    setTimeout(function() {
-        document.getElementById("canvas_three").classList.add('animated')
-    }, 200);
 }
--- www/js/new-age.js
+++ www/js/new-age.js
@@ -26,6 +26,44 @@
     offset: 54
   });
 
+  // Change logo, and collapse the navbar when page is scrolled
+  var changeOnScroll = function() {
+    changeLogo();
+    navbarCollapse();
+  };
+
+  // Check if element is visible, used when scrolling
+  var isElementInView = function (element, fullyInView) {
+        var pageTop = $(window).scrollTop();
+        var pageBottom = pageTop + $(window).height();
+        var elementTop = $(element).offset().top;
+        var elementBottom = elementTop + $(element).height();
+
+        if (fullyInView === true) {
+            return ((pageTop < elementTop) && (pageBottom > elementBottom));
+        } else {
+            return ((elementTop <= pageBottom) && (elementBottom >= pageTop));
+        }
+  }
+
+  // Change the logo-image when scrolling over different sections
+  // Sections: planet_container, download, features, timeline, faqs, vision, contact
+  var changeLogo = function() {
+    if (isElementInView($('#planet_container'), false)) {
+      $("#logo-image").attr("src","img/logo.png");
+    } else if (isElementInView($('#download'), false)) {
+      $("#logo-image").attr("src","img/p1.png");
+    } else if (isElementInView($('#cta'), false)) {
+      $("#logo-image").attr("src","img/p2.png");
+    } else if (isElementInView($('#timeline'), false)) {
+      $("#logo-image").attr("src","img/p3.png");
+    } else if (isElementInView($('#vision'), false)) {
+      $("#logo-image").attr("src","img/p4.png");
+    } else if (isElementInView($('#contact'), false)) {
+      $("#logo-image").attr("src","img/p5.png");
+    }
+  };
+
   // Collapse Navbar
   var navbarCollapse = function() {
     if ($("#mainNav").offset().top > 100) {
@@ -38,8 +76,9 @@
   };
   // Collapse now if page is not at top
   navbarCollapse();
-  // Collapse the navbar when page is scrolled
-  $(window).scroll(navbarCollapse);
+
+  // Change things on scroll
+  $(window).scroll(changeOnScroll);
   
   // Set the copyright year
   var currentYear = (new Date).getFullYear(),
